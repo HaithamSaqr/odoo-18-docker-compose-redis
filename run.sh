fbaea51d0c0e40de3321d19b0040fda1aff46b54
HaithamSaqr <info@falcon-v.com>
2025-03-13 01:01:19 +0200

    up

--- run.sh
+++ run.sh
@@ -44,11 +44,6 @@ find $DESTINATION -type f -exec chmod 644 {} \;
 find $DESTINATION -type d -exec chmod 755 {} \;
 
 # Run Odoo
-docker-compose -f $DESTINATION/docker-compose.yml pull
 docker-compose -f $DESTINATION/docker-compose.yml up -d
-
-#sudo chmod -R 777 $DESTINATION/addons
-#sudo chmod -R 777 $DESTINATION/etc
-#sudo chmod -R 777 $DESTINATION/postgresql
 
 echo "Odoo started at http://localhost:$PORT | Master Password: P@ss@123 | Live chat port: $CHAT"
